387853996be2428e37913a274ddba329abdbaa9b
Evellynsnt <159397519+Evellynsnt@users.noreply.github.com>
2025-02-27 08:17:23 -0300

    Update ManipularCarro1.java

--- ManipularCarro1.java
+++ ManipularCarro1.java
@@ -6,26 +6,33 @@ public class ManipularCarro1 {
     public ManipularCarro1(String marca, String modelo, String ano){
         this.marca = marca;
         this.modelo = modelo;
-        this.ano = ano;
-
-        public String getMarca{
-            return marca;
-        }
-        public String getModelo{
-            return modelo;
-        }
-        public String getAno{
-            return ano;
-        }
-
-        public void setMarca(String marca){
-            this.marca;
-        }
-        public void setModelo(String modelo){
-            this.modelo;
-        }
-        public void setAno(String ano){
-            this.ano;
-        }
+        this.ano = ano; 
     }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getAno() {
+        return ano;
+    }
+
+    public void setAno(String ano) {
+        this.ano = ano;
+    }
+
+    
+
 }
